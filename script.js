efa23f396fe6054c3c21ae5020488b1775c24593
fatoumcodes <fatsashkir12@gmail.com>
2026-09-21 20:51:32 +0100

    Build Pomodoro timer countdown

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const widget = document.getElementById('mood');
+const widget = document.getElementById('sakura-widget');
+const timer = document.getElementById ('timer')
+const button = document.getElementById ('timer-button')
 
 let isDragging = false;
+let offsetX;
+let offsetY;
+let timeLeft = 1500;
+
+
+button.addEventListener ('click', () => {
+    setInterval (() => {    
+        timeLeft = timeLeft - 1;
+
+        const minutes = Math.floor (timeLeft/60);
+        const seconds = timeLeft % 60;
+
+        timer.textContent = `${minutes}: ${seconds.toString ().padStart (2, '0')}`;
+    }, 1000);
+});
 
 widget.addEventListener('mousedown', (e) => {
 
-    isDragging = true;
+    if (e.target.tagName !== 'BUTTON') {
+
+        isDragging = true;
+
+        offsetX = e.offsetX;
+        offsetY = e.offsetY;
+
+    }
 
 });
 
@@ -12,8 +36,8 @@ document.addEventListener('mousemove', (e) => {
 
     if (isDragging) {
 
-        widget.style.left = e.clientX + 'px';
-        widget.style.top = e.clientY + 'px';
+        widget.style.left = e.clientX - offsetX + 'px';
+        widget.style.top = e.clientY - offsetY + 'px';
 
     }
 
